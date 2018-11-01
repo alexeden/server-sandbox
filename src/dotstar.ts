319f0954914f6fef0162ff14351a47a9004d3de0
Alex Eden <alexandereden91@gmail.com>
2018-11-01 01:34:59 -0500

    Allow start/end frame counts to be repeated, write some setter methods

--- src/dotstar.ts
+++ src/dotstar.ts
@@ -1,4 +1,4 @@
-import { clamp } from 'ramda';
+import { clamp, range } from 'ramda';
 import * as SPI from 'pi-spi';
 import { APA102C } from './apa102c';
 
@@ -6,35 +6,62 @@ export interface DotstarConfig {
   clockSpeed: number;
   devicePath: string;
   leds: number;
+  startFrames: number,
+  endFrames: number
 }
 
 export class Dotstar {
   static async create(config: Partial<DotstarConfig> = {}): Promise<Dotstar> {
     const spi = SPI.initialize(config.devicePath || '/dev/spidev0.0');
-    spi.clockSpeed(clamp(APA102C.CLK_MIN, APA102C.CLK_MAX, config.clockSpeed || 800_000));
-    return new Dotstar(spi, Math.max(0, config.leds || 144));
+    spi.clockSpeed(clamp(APA102C.CLK_MIN, APA102C.CLK_MAX, config.clockSpeed || APA102C.CLK_MIN));
+    return new Dotstar(spi, Math.max(0, config.leds || 144), config.startFrames || 1, config.endFrames || 1);
   }
 
   private readonly buffer: Buffer;
-  readonly frames: number;
+  private readonly ledSlices: Buffer[];
+  readonly totalFrames: number;
   readonly bufferSize: number;
 
   private constructor(
     private readonly spi: SPI.SPI,
-    readonly leds: number
+    readonly leds: number,
+    readonly startFrames: number,
+    readonly endFrames: number
   ) {
-    this.frames = this.leds + 2;
-    this.bufferSize = APA102C.FRAME_SIZE * this.frames;
+    this.totalFrames = this.leds + this.startFrames + this.endFrames;
+    this.bufferSize = APA102C.FRAME_SIZE * this.totalFrames;
     this.buffer = Buffer.concat([
-      APA102C.startFrame(),
-      APA102C.endFrame(),
+      ...range(0, this.startFrames).map(APA102C.startFrame),
+      ...range(0, this.leds).map(APA102C.ledFrame),
+      ...range(0, this.endFrames).map(APA102C.startFrame),
     ]);
+
+    this.ledSlices = range(0, this.leds)
+      .map(i => (i + this.startFrames) * APA102C.FRAME_SIZE)
+      .map(offset => this.buffer.slice(offset, offset + 3));
   }
 
   get clockSpeed() {
     return this.spi.clockSpeed();
   }
 
+  set(color: number, ...is: number[]) {
+    is.filter(i => i >= 0 && i < this.leds).forEach(i => {
+      this.ledSlices[i].writeUIntBE(color, 0, 3);
+    });
+  }
+
+  setAll(color: number) {
+    this.ledSlices.forEach(slice => slice.writeUIntBE(color, 0, 3));
+  }
+
+  apply(fn: (color: number, index: number) => number) {
+    this.ledSlices.forEach((led, i) => {
+      const color = led.readUIntBE(0, 3);
+      const newColor = fn(color, i);
+      led.writeUIntBE(newColor, 0, 3);
+    });
+  }
 
   async sync(): Promise<any> {
     return new Promise((ok, err) => {
@@ -45,12 +72,11 @@ export class Dotstar {
     });
   }
 
-
   printBuffer(): string {
     const segments: string[] = [];
     let segment: string[] = [];
 
-    for (let i = 0; i <= this.buffer.byteLength; i++) {
+    for (let i = 0; i <= this.bufferSize; i++) {
       if (i > 0 && i % 4 === 0) {
         segments.push(segment.join('\t'));
         segment = [];
