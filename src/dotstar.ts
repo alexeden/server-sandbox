85868d6d0a5cce0da22589280bb248e801fdb08f
Alex Eden <alexandereden91@gmail.com>
2018-11-01 03:30:22 -0500

    Yayyy it's working nicely

--- src/dotstar.ts
+++ src/dotstar.ts
@@ -1,19 +1,21 @@
-import { clamp, range } from 'ramda';
+import { range } from 'ramda';
 import * as SPI from 'pi-spi';
 import { APA102C } from './apa102c';
 
 export interface DotstarConfig {
   clockSpeed: number;
+  dataMode: SPI.mode;
   devicePath: string;
+  endFrames: number
   leds: number;
   startFrames: number,
-  endFrames: number
 }
 
 export class Dotstar {
   static async create(config: Partial<DotstarConfig> = {}): Promise<Dotstar> {
     const spi = SPI.initialize(config.devicePath || '/dev/spidev0.0');
-    spi.clockSpeed(clamp(APA102C.CLK_MIN, APA102C.CLK_MAX, config.clockSpeed || APA102C.CLK_MIN));
+    spi.clockSpeed(Math.max(APA102C.CLK_MIN, config.clockSpeed || APA102C.CLK_MIN));
+    spi.dataMode(typeof config.dataMode === 'number' ? config.dataMode : 0);
     return new Dotstar(spi, Math.max(0, config.leds || 144), config.startFrames || 1, config.endFrames || 1);
   }
 
@@ -45,6 +47,10 @@ export class Dotstar {
     return this.spi.clockSpeed();
   }
 
+  get dataMode() {
+    return this.spi.dataMode();
+  }
+
   set(color: number, ...is: number[]) {
     is.filter(i => i >= 0 && i < this.leds).forEach(i => {
       this.write(this.ledSlices[i], color);
@@ -70,13 +76,22 @@ export class Dotstar {
 
   off() {
     this.setAll(0);
+    this.syncing = false;
     return this.sync();
   }
 
+  private syncing = false;
+
   async sync(): Promise<any> {
+    if (this.syncing) return;
+    this.syncing = true;
     return new Promise((ok, err) => {
       this.spi.write(this.buffer, (error, data) => {
-        if (error) err(error);
+        this.syncing = false;
+        if (error) {
+          console.error(error);
+          err(error);
+        }
         else ok(data);
       });
     });
@@ -98,19 +113,3 @@ export class Dotstar {
     return segments.join('\n');
   }
 }
-
-// export const spi = SPI.initialize(SPI_PORT);
-// spi.clockSpeed(800_000);
-
-// const toFrame = (r: number, g: number, b: number) => flatten([
-//   ...range(0, 4).map(_ => START_FRAME_BYTE),
-//   ...range(0, N).map(_ => [r, g, b, 0xFF]),
-//   ...range(0, 4).map(_ => [0xFF, 0xFF, 0xFF, 0xFF]),
-// ]);
-
-// export const write = ([r, g, b]: [number, number, number]) => {
-//   const frame = toFrame(r, g, b);
-//   spi.write(Buffer.from(frame), (error, data) => {
-//     if (error) console.error('error: ', error);
-//   });
-// };
--- src/index.ts
+++ src/index.ts
@@ -2,67 +2,37 @@ import { Dotstar } from './dotstar';
 import { Colors } from './colors';
 import * as OS from 'os';
 import chalk from 'chalk';
+import { APA102C } from './apa102c';
 
 (async () => {
-  const dotstar = await Dotstar.create({
-    devicePath: OS.type() !== 'Linux' ? '/dev/null' : undefined,
-    // startFrames: 1,
-    // endFrames: 4,
-  });
+  try {
+    const dotstar = await Dotstar.create({
+      devicePath: OS.type() !== 'Linux' ? '/dev/null' : undefined,
+      clockSpeed: APA102C.CLK_MAX,
+      endFrames: 4,
+    });
 
-  const colors = Object.values(Colors).filter(c => typeof c === 'number');
 
-  console.log(colors);
-  // const testInterval = setInterval(
-  //   () => {
-  //     dotstar.apply((c, i) => colors[Math.floor(Math.random() * colors.length)]);
-  //     dotstar.sync();
-  //   },
-  //   50
-  // );
+    const colors = Object.values(Colors).filter(c => typeof c === 'number');
+    const testInterval = setInterval(
+      () => {
+        dotstar.apply((c, i) => colors[Math.floor(Math.random() * colors.length)]);
+        dotstar.sync();
+      },
+      5
+    );
 
-  const set = async (cs: number[]) => {
-    if (cs.length <= 0) {
-      console.log(chalk.blue(dotstar.printBuffer()));
-      dotstar.off();
-      // dotstar.off();
-      return;
-    }
-    // dotstar.apply((c, i) => colors[Math.floor(Math.random() * colors.length)]);
-    console.log(`Setting color to "${Colors[cs[0]]}": 0x${cs[0].toString(16)}`);
-    dotstar.setAll(cs[0]);
-    await dotstar.sync();
-    if (cs.length > 0) {
-      setTimeout(() => set(cs.slice(1)), 2000);
-    }
-  };
-
-  console.log(`writing these colors: ${colors.map(c => Colors[c]).join(', ')}`);
-  set([...colors]); // , ...colors, ...colors, ...colors, ...colors, ...colors, ...colors, ...colors, ...colors, ...colors]);
-  // )
-  // setTimeout(
-  //   () => {
-  //     // clearInterval(testInterval);
-  //     dotstar.off();
-  //     console.log(chalk.blue(dotstar.printBuffer()));
-  //     console.log(chalk.green('done! everything should be off'));
-  //   },
-  //   15000
-  // );
+    setTimeout(
+      () => {
+        clearInterval(testInterval);
+        dotstar.off();
+        console.log(chalk.green('done! everything should be off'));
+      },
+      5000
+    );
+  }
+  catch (error) {
+    console.error(error);
+  }
 
 })();
-
-
-
-// const shutdown = () => {
-//   console.log('shutting down');
-//   write([0, 0, 0]);
-// };
-// setTimeout(() => write([255, 0, 0]), 0);
-// setTimeout(() => write([255, 0x7f, 0]), 1000);
-// setTimeout(() => write([0, 255, 0]), 2000);
-// setTimeout(() => spi.close(), 2500);
-// setTimeout(() => write([0, 255, 255]), 3000);
-// setTimeout(() => write([0, 0, 255]), 4000);
-// setTimeout(() => write([255, 0, 255]), 5000);
-// setTimeout(shutdown, 6000);
