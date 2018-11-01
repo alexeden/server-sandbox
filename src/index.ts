9d026fd0271d2f87370bb49590420bb1375cc20b
Alex Eden <alexandereden91@gmail.com>
2018-10-31 23:28:54 -0500

    Use /dev/null as the device path if not on linux (for local testing)

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
-import { write, spi } from './dotstar';
+import { Dotstar } from './dotstar';
+import * as OS from 'os';
 
-const shutdown = () => {
-  console.log('shutting down');
-  write([0, 0, 0]);
-};
-setTimeout(() => write([255, 0, 0]), 0);
-setTimeout(() => write([255, 0x7f, 0]), 1000);
-setTimeout(() => write([0, 255, 0]), 2000);
-setTimeout(() => spi.close(), 2500);
-setTimeout(() => write([0, 255, 255]), 3000);
-setTimeout(() => write([0, 0, 255]), 4000);
-setTimeout(() => write([255, 0, 255]), 5000);
-setTimeout(shutdown, 6000);
+
+const dotstar = Dotstar.create({
+  devicePath: OS.type() !== 'Linux' ? '/dev/null' : undefined,
+});
+
+
+
+// const shutdown = () => {
+//   console.log('shutting down');
+//   write([0, 0, 0]);
+// };
+// setTimeout(() => write([255, 0, 0]), 0);
+// setTimeout(() => write([255, 0x7f, 0]), 1000);
+// setTimeout(() => write([0, 255, 0]), 2000);
+// setTimeout(() => spi.close(), 2500);
+// setTimeout(() => write([0, 255, 255]), 3000);
+// setTimeout(() => write([0, 0, 255]), 4000);
+// setTimeout(() => write([255, 0, 255]), 5000);
+// setTimeout(shutdown, 6000);
