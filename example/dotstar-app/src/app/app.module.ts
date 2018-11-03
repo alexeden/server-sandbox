c7b893c7e625ac1d8566b361d848173e8679f918
Alex Eden <alexandereden91@gmail.com>
2018-11-03 00:41:09 -0500

    Wire it all up

--- example/dotstar-app/src/app/app.module.ts
+++ example/dotstar-app/src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
+import { AppMaterialModule } from './material.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserAnimationsModule,
+    AppMaterialModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ example/dotstar-app/src/app/material.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+
+@NgModule({
+  exports: [MatButtonModule, MatCheckboxModule],
+})
+export class AppMaterialModule { }
