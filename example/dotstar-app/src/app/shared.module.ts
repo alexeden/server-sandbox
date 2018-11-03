ff69f09961cc8aac98420e43fb525617becef79e
Alex Eden <alexandereden91@gmail.com>
2018-11-03 02:30:51 -0500

    Import reactive forms and more mat module

--- example/dotstar-app/src/app/shared.module.ts
+++ example/dotstar-app/src/app/shared.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+} from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   exports: [
     CommonModule,
+    ReactiveFormsModule,
     MatButtonModule,
     MatCheckboxModule,
+    MatExpansionModule,
+    MatFormFieldModule,
   ],
 })
 export class SharedModule { }
