23a67aa16034920a34cab0e70e6019b2a8ac607f
Alex Eden <alexandereden91@gmail.com>
2018-11-03 23:28:58 -0600

    Setup ping-pong socket tracking

--- example/server/src/index.ts
+++ example/server/src/index.ts
@@ -5,22 +5,31 @@ import * as fs from 'fs';
 import * as net from 'net';
 import * as websockets from 'ws';
 
-const httpsServerOptions: https.ServerOptions = {
-  key: fs.readFileSync(path.resolve(__dirname, '..', 'server.key')),
-  cert: fs.readFileSync(path.resolve(__dirname, '..', 'server.crt')),
-};
+const server = https.createServer({
+    key: fs.readFileSync(path.resolve(__dirname, '..', 'server.key')),
+    cert: fs.readFileSync(path.resolve(__dirname, '..', 'server.crt')),
+  })
+  .listen(4000, '0.0.0.0', () => console.log('listening on port 4000'));
 
-const server = https.createServer(httpsServerOptions);
-const wsServer = new websockets.Server({ noServer: true });
-
-wsServer.on('connection', socket => {
-  console.log('got a connection!');
-});
+const wss = new websockets.Server({ noServer: true });
 
 server.on('upgrade', (request: http.IncomingMessage, socket: net.Socket, head: Buffer) => {
-  wsServer.handleUpgrade(request, socket, head, clientSocket => {
-    wsServer.emit('connection', clientSocket, request);
-  });
+  wss.handleUpgrade(request, socket, head, clientSocket => wss.emit('connection', clientSocket, request));
 });
 
-server.listen(4000, '0.0.0.0', () => console.log('listening on port 4000'));
+const liveClients = new Set<websockets>([]);
+
+wss.on('connection', socket => {
+  console.log('got a connection!');
+  liveClients.add(socket);
+  socket.on('pong', liveClients.add.bind(liveClients, socket));
+});
+
+setInterval(
+  () => wss.clients.forEach(socket => {
+    if (!liveClients.has(socket)) socket.terminate();
+    liveClients.delete(socket);
+    socket.ping(() => {});
+  }),
+  5000
+);
