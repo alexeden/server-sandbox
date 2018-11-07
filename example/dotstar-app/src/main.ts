7097703cddec61dc008d7d39888538ab2eb063d4
Alex Eden <alexandereden91@gmail.com>
2018-11-07 00:16:54 -0600

    Install hammerjs

--- example/dotstar-app/src/main.ts
+++ example/dotstar-app/src/main.ts
@@ -1,3 +1,4 @@
+import 'hammerjs'; // tslint:disable-line:no-import-side-effect
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
