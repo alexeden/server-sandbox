b289e73d776435bb6599da479a6dbeef817fb2b5
Alex Eden <alexandereden91@gmail.com>
2018-11-14 00:49:22 -0600

    Suddenly I remember why I gave up C++

--- binding.gyp
+++ binding.gyp
@@ -1,26 +1,29 @@
 {
   "targets": [
-    {
-      "target_name": "spi_napi",
-      "sources": [
-        "src/spi_napi.cc"
-      ],
-      "cflags": [ '-Wall' ],
-      'cflags!': [ '-fno-exceptions' ],
-      'cflags_cc!': [ '-fno-exceptions' ],
-      "include_dirs": [
-        "<!@(node -p \"require('node-addon-api').include\")"
-      ],
-      'defines': [ 'NAPI_DISABLE_CPP_EXCEPTIONS' ],
-    },
     # {
-    #   "target_name": "spi_binding",
+    #   "target_name": "spi_napi",
     #   "sources": [
-    #     "src/spi_binding.cc"
+    #     "src/spi_napi.cc"
     #   ],
-    #   "include_dirs" : [
-    #     '<!(node -e "require(\'nan\')")'
-    #   ]
-    # }
+    #   "cflags": [ '-Wall' ],
+    #   'cflags!': [ '-fno-exceptions' ],
+    #   'cflags_cc!': [ '-fno-exceptions' ],
+    #   "include_dirs": [
+    #     "<!@(node -p \"require('node-addon-api').include\")"
+    #   ],
+    #   'dependencies': [
+    #     "<!(node -p \"require('node-addon-api').gyp\")"
+    #   ],
+    #   'defines': [ 'NAPI_DISABLE_CPP_EXCEPTIONS' ],
+    # },
+    {
+      "target_name": "spi_binding",
+      "sources": [
+        "src/spi_binding.cc"
+      ],
+      "include_dirs" : [
+        '<!(node -e "require(\'nan\')")'
+      ]
+    }
   ]
 }
--- src/spi_napi.cc
+++ src/spi_napi.cc
@@ -5,80 +5,96 @@
 
 // #define DECLARE_METHOD(name, func) { name, NULL, func, NULL, NULL, NULL, napi_enumerable, NULL }
 
-class EchoWorker : public Napi::AsyncWorker {
-	private:
-        std::string echo;
+#if __linux__
+#include <linux/types.h>
+#include <sys/ioctl.h>
+#include <linux/spi/spidev.h>
+#endif
 
-    public:
-        EchoWorker(
-			Napi::Function& callback,
-			std::string& echo
-		): AsyncWorker(callback), echo(echo) {}
+/**
+ * struct spi_ioc_transfer {
+ *		__u64		tx_buf;
+ *		__u64		rx_buf;
+ *		__u32		len;
+ *		__u32		speed_hz;
+ *		__u16		delay_usecs;
+ *		__u8		bits_per_word;
+ *		__u8		cs_change;
+ *		__u8		tx_nbits;
+ *		__u8		rx_nbits;
+ *		__u16		pad;
+ * };
+ */
+#ifndef SPI_IOC_MESSAGE
+struct spi_ioc_transfer {
+	uintptr_t		tx_buf;
+	uintptr_t		rx_buf;
+	size_t			len;
+	uint32_t		speed_hz;
+};
+#endif
 
-        ~EchoWorker() {}
+class SpiTransfer : public Napi::AsyncWorker {
+private:
+	std::string echo;
+	int fd;
+	int err;
+	// uint32_t speed;
+	// uint8_t mode;
+	// uint8_t order;
+	// uint32_t readcount;
+	size_t buflen;
+	Napi::Buffer<uint8_t>* buffer;
 
-    void Execute() {
+public:
+	SpiTransfer(
+		Napi::Function& callback,
+		Napi::Buffer<uint8_t>* buffer,
+		int fd
+		// uint32_t speed,
+		// uint8_t mode,
+		// uint8_t order,
+		// size_t readcount
+	): 	AsyncWorker(callback),
+		buffer(buffer),
+		fd(fd)
+		// speed(speed),
+		// mode(mode),
+		// order(order),
+		// readcount(readcount)
+	{
+
+	}
+
+	~SpiTransfer() {}
+
+	void Execute() {
 		// quick nap
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-    }
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+	}
 
-    void OnOK() {
-        Napi::HandleScope scope(Env());
-        Callback().Call({Env().Null(), Napi::String::New(Env(), echo)});
-    }
+	void OnOK() {
+		Napi::HandleScope scope(Env());
+		Callback().Call({Env().Null(), Napi::String::New(Env(), "hi")});
+	}
+
+	void OnError(const Napi::Error& e) {
+
+	}
 };
 
-class EchoPromiseWorker : public Napi::AsyncWorker {
-	private:
-        std::string echo;
-		Napi::Promise::Deferred deferred;
-    public:
-        EchoPromiseWorker(
-			Napi::Function& callback,
-			Napi::Promise::Deferred& deferred,
-			std::string& echo
-		): AsyncWorker(callback), deferred(deferred), echo(echo) {}
-
-        ~EchoPromiseWorker() {}
-
-    void Execute() {
-		// quick nap
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-    }
-
-    void OnOK() {
-        Napi::HandleScope scope(Env());
-		deferred.Resolve(NULL);
-        // Callback().Call({});
-    }
-};
-
-Napi::String Hello(const Napi::CallbackInfo& info) {
-	Napi::Env env = info.Env();
-	return Napi::String::New(env, "world");
-}
-
-Napi::Value Echo(const Napi::CallbackInfo& info) {
-	std::string in = info[0].As<Napi::String>();
-	Napi::Function cb = info[1].As<Napi::Function>();
-	EchoWorker *worker = new EchoWorker(cb, in);
+Napi::Value Transfer(const Napi::CallbackInfo& info) {
+	Napi::Buffer<uint8_t> buffer = info[0].As<Napi::Buffer<uint8_t>>();
+	int fd = info[1].As<Napi::Number>();
+	Napi::Function cb = info[2].As<Napi::Function>();
+	SpiTransfer *worker = new SpiTransfer(cb, &buffer, fd);
 	worker->Queue();
 	return info.Env().Undefined();
 }
 
-Napi::Promise EchoPromise(const Napi::CallbackInfo& info) {
-	std::string in = info[0].As<Napi::String>();
-	Napi::Function cb = Napi::Function::New(info.Env(), Napi::Function());
-	Napi::Promise::Deferred deferred = Napi::Promise::Deferred::New(info.Env());
-	EchoPromiseWorker *worker = new EchoPromiseWorker(cb, deferred, in);
-	worker->Queue();
-	return deferred.Promise();
-}
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
-	exports.Set(Napi::String::New(env, "hello"), Napi::Function::New(env, Hello));
-	exports.Set(Napi::String::New(env, "echo"), Napi::Function::New(env, Echo));
-	exports.Set(Napi::String::New(env, "echoPromise"), Napi::Function::New(env, EchoPromise));
+	// exports.Set(Napi::String::New(env, "echo"), Napi::Function::New(env, Echo));
 	return exports;
 }
 
