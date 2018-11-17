636fe5385e24e4162606fd6d1cb8dce2e9550bcc
Alex Eden <alexandereden91@gmail.com>
2018-11-17 01:29:23 -0600

    Final cleanup

--- example/dotstar-app/src/app/dotstar/lib/constants.ts
+++ example/dotstar-app/src/app/dotstar/lib/constants.ts
@@ -6,7 +6,6 @@ export class DotstarConstants {
   static readonly rSampler = '(255/2) * Math.sin(2 * t/1000) *  Math.sin((Math.PI * 2 / n) * i - t / 2000) + (255/2)';
   static readonly gSampler = '(255 / 2) * Math.sin((Math.PI * 2 / n) * i + 2 * t / 1000) + (255 / 2)';
   static readonly bSampler = '255 * Math.sin(i / 15 - (Math.PI * 2 / n) * 50*t/1000)';
-  // static readonly hSampler = '360 * (i / n - t/5000)';
   static readonly hSampler = '[0, 8, 25, 120, 240, 300][Math.floor(6*i/n)]';
   static readonly sSampler = '1';
   static readonly lSampler = '0.5';
