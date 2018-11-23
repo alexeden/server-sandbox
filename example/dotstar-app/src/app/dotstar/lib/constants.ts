2917d440a641e91bd570bbedaec0a9bc4fa9f9f2
Alex Eden <alexandereden91@gmail.com>
2018-11-23 04:45:28 -0600

    Reverse the default hue sampler function

--- example/dotstar-app/src/app/dotstar/lib/constants.ts
+++ example/dotstar-app/src/app/dotstar/lib/constants.ts
@@ -6,7 +6,8 @@ export class DotstarConstants {
   static readonly rSampler = '(255/2) * Math.sin(2 * t/1000) *  Math.sin((Math.PI * 2 / n) * i - t / 2000) + (255/2)';
   static readonly gSampler = '(255 / 2) * Math.sin((Math.PI * 2 / n) * i + 2 * t / 1000) + (255 / 2)';
   static readonly bSampler = '255 * Math.sin(i / 15 - (Math.PI * 2 / n) * 50*t/1000)';
-  static readonly hSampler = '[0, 8, 25, 120, 240, 330][Math.floor(6*i/n)]';
+  // static readonly hSampler = '[0, 9, 25, 120, 240, 330][Math.floor(6*i/n)]';
+  static readonly hSampler = '[330, 240, 120, 25, 9, 0][Math.floor(6*i/n)]';
   static readonly sSampler = '1';
   static readonly lSampler = '0.5 * (Math.sin(t/500 - i/n) > 0 ? 1 : 0)';
 }
