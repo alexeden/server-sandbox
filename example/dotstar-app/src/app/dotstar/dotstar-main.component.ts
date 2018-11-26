bab93c92d9fea047bc64cac45a0907490c033b26
Alex Eden <alexandereden91@gmail.com>
2018-11-25 23:03:29 -0600

    Add a tabbed nav bar

--- example/dotstar-app/src/app/dotstar/dotstar-main.component.ts
+++ example/dotstar-app/src/app/dotstar/dotstar-main.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, Inject } from '@angular/core';
 import { Subject } from 'rxjs';
+import { ROUTES, Route } from '@angular/router';
 
 @Component({
   selector: 'dotstar-main',
@@ -8,6 +9,19 @@ import { Subject } from 'rxjs';
 })
 export class DotstarMainComponent implements OnInit, OnDestroy {
   private readonly unsubscribe$ = new Subject<any>();
+  readonly dotstarRoutes: Route[];
+
+  constructor(
+    @Inject(ROUTES) public injectedRoutes: Route[][]
+  ) {
+    const dotstarParentRoute = this.injectedRoutes[0].find(r => r.path === 'dotstar');
+
+    if (!dotstarParentRoute) {
+      throw new Error(`No parent route wth path "/dotstar" found!`);
+    }
+
+    this.dotstarRoutes = (dotstarParentRoute.children || []).filter(r => r.path.length > 0);
+  }
 
   ngOnInit() {
     //
