18653c66790148b13654aeb17dead26745c373b4
Alex Eden <alexandereden91@gmail.com>
2018-11-25 22:48:52 -0600

    Make the control bar component functions self-contained

--- example/dotstar-app/src/app/dotstar/control-bar/control-bar.component.ts
+++ example/dotstar-app/src/app/dotstar/control-bar/control-bar.component.ts
@@ -1,8 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostBinding } from '@angular/core';
+import { DotstarBufferService } from '../dotstar-buffer.service';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { DotstarDeviceConfigService } from '../device-config.service';
+import { DotstarSocketService } from '../dotstar-socket.service';
+import { AnimationClockService } from '../animation-clock.service';
+import { Subject, Observable } from 'rxjs';
+import { DotstarConstants } from '../lib';
+import { map, takeUntil, startWith, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'dotstar-control-bar',
   templateUrl: './control-bar.component.html',
   styleUrls: ['./control-bar.component.scss'],
 })
-export class DotstarControlBarComponent {}
+export class DotstarControlBarComponent implements OnInit, OnDestroy {
+  @HostBinding('attr.class') classes = 'row gap-40';
+  private readonly unsubscribe$ = new Subject<any>();
+
+  readonly txpsControl: FormControl;
+  readonly txpsMin = DotstarConstants.txpsMin;
+  readonly txpsMax = DotstarConstants.txpsMax;
+  readonly devicePath: Observable<string>;
+
+  constructor(
+    private fb: FormBuilder,
+    readonly bufferService: DotstarBufferService,
+    readonly configService: DotstarDeviceConfigService,
+    readonly socketService: DotstarSocketService,
+    readonly clock: AnimationClockService
+  ) {
+    this.txpsControl = this.fb.control(60, [
+      Validators.min(this.txpsMin),
+      Validators.max(this.txpsMax),
+    ]);
+
+    this.devicePath = this.configService.deviceConfig.pipe(
+      map(config => config.devicePath)
+    );
+  }
+
+  ngOnInit() {
+    this.txpsControl.valueChanges.pipe(
+      takeUntil(this.unsubscribe$),
+      startWith(this.txpsControl.value),
+      filter(() => this.txpsControl.valid)
+    )
+    .subscribe(txps => this.socketService.setTxps(txps));
+
+    this.configService.getAvailableDevicePaths();
+  }
+
+
+  ngOnDestroy() {
+    this.unsubscribe$.next('unsubscribe!');
+    this.unsubscribe$.unsubscribe();
+  }
+}
--- example/dotstar-app/src/app/dotstar/dotstar-main.component.ts
+++ example/dotstar-app/src/app/dotstar/dotstar-main.component.ts
@@ -1,12 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { DotstarBufferService } from './dotstar-buffer.service';
-import { DotstarDeviceConfigService } from './device-config.service';
-import { DotstarSocketService } from './dotstar-socket.service';
-import { DotstarConstants } from './lib';
-import { FormControl, FormBuilder, Validators } from '@angular/forms';
-import { takeUntil, startWith, filter, map } from 'rxjs/operators';
-import { Subject, Observable } from 'rxjs';
-import { AnimationClockService } from './animation-clock.service';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'dotstar-main',
@@ -16,44 +9,12 @@ import { AnimationClockService } from './animation-clock.service';
 export class DotstarMainComponent implements OnInit, OnDestroy {
   private readonly unsubscribe$ = new Subject<any>();
 
-  readonly txpsControl: FormControl;
-  readonly txpsMin = DotstarConstants.txpsMin;
-  readonly txpsMax = DotstarConstants.txpsMax;
-  readonly devicePath: Observable<string>;
-
-
-  constructor(
-    private fb: FormBuilder,
-    readonly bufferService: DotstarBufferService,
-    readonly configService: DotstarDeviceConfigService,
-    readonly socketService: DotstarSocketService,
-    readonly clock: AnimationClockService
-  ) {
-    this.txpsControl = this.fb.control(60, [
-      Validators.min(this.txpsMin),
-      Validators.max(this.txpsMax),
-    ]);
-
-    this.devicePath = this.configService.deviceConfig.pipe(
-      map(config => config.devicePath)
-    );
-  }
-
   ngOnInit() {
-    this.txpsControl.valueChanges.pipe(
-      takeUntil(this.unsubscribe$),
-      startWith(this.txpsControl.value),
-      filter(() => this.txpsControl.valid)
-    )
-    .subscribe(txps => this.socketService.setTxps(txps));
-
-    this.configService.getAvailableDevicePaths();
+    //
   }
-
 
   ngOnDestroy() {
     this.unsubscribe$.next('unsubscribe!');
     this.unsubscribe$.unsubscribe();
   }
-
 }
