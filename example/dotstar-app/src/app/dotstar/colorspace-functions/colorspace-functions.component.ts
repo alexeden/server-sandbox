a610fafaf6a0311edfd32f73329f8c968178337d
Alex Eden <alexandereden91@gmail.com>
2018-11-26 02:27:54 -0600

    Scaffold two components to serve as the control mode wrappers

---
+++ example/dotstar-app/src/app/dotstar/colorspace-functions/colorspace-functions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-colorspace-functions',
+  templateUrl: './colorspace-functions.component.html',
+  styleUrls: ['./colorspace-functions.component.scss']
+})
+export class ColorspaceFunctionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ example/dotstar-app/src/app/dotstar/pointer-particles/pointer-particles.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pointer-particles',
+  templateUrl: './pointer-particles.component.html',
+  styleUrls: ['./pointer-particles.component.scss']
+})
+export class PointerParticlesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
