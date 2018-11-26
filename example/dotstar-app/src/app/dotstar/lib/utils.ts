95ef36f0acff0b2e6724b58aa3fa14aff731967e
Alex Eden <alexandereden91@gmail.com>
2018-11-26 00:55:33 -0600

    Add an affine transform util function

--- example/dotstar-app/src/app/dotstar/lib/utils.ts
+++ example/dotstar-app/src/app/dotstar/lib/utils.ts
@@ -1,10 +1,7 @@
 export const range = (from: number, to: number) => {
   const result = [];
-  let n = from;
-  while (n < to) {
-    result.push(n);
-    n += 1;
-  }
+  let n = from - 1;
+  while (++n < to) result.push(n);
   return result;
 };
 
@@ -20,4 +17,13 @@ export const clampLoop = (min: number, max: number) => (value: number) => {
   else if (a < min) a += len;
 
   return a;
+};
+
+export const absDiff = (a: number, b: number) => Math.abs(b - a);
+
+export const normalize = (a: number, b: number, x: number) => (x - Math.min(a, b)) / absDiff(a, b);
+
+export const mapToRange = (a: number, b: number, c: number, d: number, x: number) => {
+  const r = (d - c) / (b - a);
+  return (x - a) * r + c;
 };
