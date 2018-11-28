bbfc05298890ce3ebf2cdd3cd933ad3466be6aee
Alex Eden <alexandereden91@gmail.com>
2018-11-27 22:32:54 -0600

    Curry the util functions

--- example/dotstar-app/src/app/dotstar/lib/utils.ts
+++ example/dotstar-app/src/app/dotstar/lib/utils.ts
@@ -1,3 +1,5 @@
+import { curryN, curry } from 'ramda';
+
 export const range = (from: number, to: number) => {
   const result = [];
   let n = from - 1;
@@ -5,25 +7,23 @@ export const range = (from: number, to: number) => {
   return result;
 };
 
-export const clamp = (min: number, max: number) => (value: number) => {
+export const clamp = curry((min: number, max: number, value: number) => {
   return Math.max(min, Math.min(max, value));
-};
+});
 
-export const clampLoop = (min: number, max: number) => (value: number) => {
+export const clampLoop = curry((min: number, max: number, value: number) => {
   const len = Math.abs(max - min);
   let a = value % len;
-
   if (a > max) a -= len;
   else if (a < min) a += len;
-
   return a;
-};
+});
 
 export const absDiff = (a: number, b: number) => Math.abs(b - a);
 
-export const normalize = (a: number, b: number, x: number) => (x - Math.min(a, b)) / absDiff(a, b);
+export const normalize = curry((a: number, b: number, x: number) => (x - Math.min(a, b)) / absDiff(a, b));
 
-export const mapToRange = (a: number, b: number, c: number, d: number, x: number) => {
+export const mapToRange = curry((a: number, b: number, c: number, d: number, x: number) => {
   const r = (d - c) / (b - a);
   return (x - a) * r + c;
-};
+});
