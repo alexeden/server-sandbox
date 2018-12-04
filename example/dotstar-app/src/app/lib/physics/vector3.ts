4d6792876761b6472a6f9480d2d215f6c651d121
Alex Eden <alexandereden91@gmail.com>
2018-12-04 02:50:50 -0600

    Finish adding the basic functions to Vector3

--- example/dotstar-app/src/app/lib/physics/vector3.ts
+++ example/dotstar-app/src/app/lib/physics/vector3.ts
@@ -1,8 +1,11 @@
 interface Triplet {
+  [Symbol.iterator]();
   [0]: number;
   [1]: number;
   [2]: number;
 }
+
+type Vector3ish = Vector3 | Triplet | number[];
 
 export class Vector3 implements Triplet {
   static empty() {
@@ -13,26 +16,120 @@ export class Vector3 implements Triplet {
     return new Vector3(values[0], values[1], values[2]);
   }
 
+  static from(vectorish: Vector3ish) {
+    return new Vector3(vectorish[0], vectorish[1], vectorish[2]);
+  }
+
   private constructor(
     readonly x: number,
     readonly y: number,
     readonly z: number
-  ) {
+  ) {}
+
+  *[Symbol.iterator]() {
+    yield this.x;
+    yield this.y;
+    yield this.z;
   }
 
-  get [0]() {
-    return this.x;
-  }
+  get [0]() { return this.x; }
+  get [1]() { return this.y; }
+  get [2]() { return this.z; }
 
-  get [1]() {
-    return this.y;
-  }
+  set0(x: number) { return this.setX(x); }
+  set1(y: number) { return this.setY(y); }
+  set2(z: number) { return this.setZ(z); }
 
-  get [2]() {
-    return this.z;
-  }
+  setX(x: number) { return new Vector3(x, this.y, this.z); }
+  setY(y: number) { return new Vector3(this.x, y, this.z); }
+  setZ(z: number) { return new Vector3(this.x, this.y, z); }
 
-  asArray(): [number, number, number] {
+  asArray(): Triplet {
     return [this.x, this.y, this.z];
+  }
+
+  clone(): Vector3 {
+    return Vector3.from(this);
+  }
+
+  magnitude(): number {
+    return Math.sqrt(this.magnitudeSquared());
+  }
+
+  magnitudeSquared(): number {
+    return this.x * this.x + this.y * this.y + this.z * this.z;
+  }
+
+  private forceVector(arg: number | Vector3ish): Vector3ish {
+    return typeof arg === 'number'
+      ? [arg, arg, arg]
+      : arg;
+  }
+
+  ceil(): Vector3 {
+    return new Vector3(Math.ceil(this.x), Math.ceil(this.y), Math.ceil(this.z));
+  }
+
+  floor(): Vector3 {
+    return new Vector3(Math.floor(this.x), Math.floor(this.y), Math.floor(this.z));
+  }
+
+  round(): Vector3 {
+    return new Vector3(Math.round(this.x), Math.round(this.y), Math.round(this.z));
+  }
+
+  add(arg: number | Vector3ish): Vector3 {
+    const [x, y, z] = this.forceVector(arg);
+    return new Vector3(this.x + x, this.y + y, this.z + z);
+  }
+
+  subtract(arg: number | Vector3ish): Vector3 {
+    const [x, y, z] = this.forceVector(arg);
+    return new Vector3(this.x - x, this.y - y, this.z - z);
+  }
+
+  multiply(arg: number | Vector3ish): Vector3 {
+    const [x, y, z] = this.forceVector(arg);
+    return new Vector3(this.x * x, this.y * y, this.z * z);
+  }
+
+  divide(arg: number | Vector3ish): Vector3 {
+    const [x, y, z] = this.forceVector(arg);
+    return new Vector3(this.x / x, this.y / y, this.z / z);
+  }
+
+  min(arg: number | Vector3ish): Vector3 {
+    const [x, y, z] = this.forceVector(arg);
+    return new Vector3(Math.min(this.x, x), Math.min(this.y, y), Math.min(this.z, z));
+  }
+
+  max(arg: number | Vector3ish): Vector3 {
+    const [x, y, z] = this.forceVector(arg);
+    return new Vector3(Math.max(this.x, x), Math.max(this.y, y), Math.max(this.z, z));
+  }
+
+  distanceFrom(v: Vector3ish): number {
+    return Math.sqrt(this.squaredDistanceFrom(v));
+  }
+
+  squaredDistanceFrom([x, y, z]: Vector3ish): number {
+    const dx = this.x - x;
+    const dy = this.y - y;
+    const dz = this.z - z;
+    return dx * dx + dy * dy + dz * dz;
+  }
+
+  negate(): Vector3 {
+    return new Vector3(-this.x, -this.y, -this.z);
+  }
+
+  invert(): Vector3 {
+    return new Vector3(1 / this.x, 1 / this.y, 1 / this.z);
+  }
+
+  normalize(): Vector3 {
+    let mag = this.magnitudeSquared();
+    if (mag > 0) mag = 1 / Math.sqrt(mag);
+    return new Vector3(this.x * mag, this.y * mag, this.z * mag);
   }
 }
