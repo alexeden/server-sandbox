670eeb0a90dceb99159c68df9cd0a0f0bc64427e
Alex Eden <alexandereden91@gmail.com>
2019-11-18 23:40:33 -0600

    Set the default device path to /dev/null

--- example/dotstar-app/src/app/device-config.service.ts
+++ example/dotstar-app/src/app/device-config.service.ts
@@ -2,13 +2,13 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { APA102C } from 'dotstar-node/dist/apa102c';
 import { DotstarConfig } from 'dotstar-node/dist/types';
-import { pluck, scan } from 'rxjs/operators';
+import { pluck, scan, tap } from 'rxjs/operators';
 
 @Injectable()
 export class DotstarDeviceConfigService {
 
   private readonly deviceConfig$ = new BehaviorSubject<DotstarConfig>({
-    devicePath: '',
+    devicePath: '/dev/null',
     length: 144,
     clockSpeed: APA102C.CLK_MAX,
     startFrames: 1,
@@ -23,7 +23,7 @@ export class DotstarDeviceConfigService {
 
   constructor() {
     this.deviceConfig = this.deviceConfig$.asObservable();
-    this.length = this.deviceConfig.pipe(pluck('length'));
+    this.length = this.deviceConfig.pipe(pluck('length'), tap(l => console.log('length: ', l)));
 
     this.devicePaths = this.devicePaths$.asObservable().pipe(
       scan((paths, newPaths) => Array.from(new Set<string>([...paths, ...newPaths])), [])
@@ -31,6 +31,7 @@ export class DotstarDeviceConfigService {
   }
 
   updateConfig(config: Partial<DotstarConfig>) {
+    console.log('updateConfig: ', config);
     this.deviceConfig$.next({
       ...this.deviceConfig$.getValue(),
       ...config,
