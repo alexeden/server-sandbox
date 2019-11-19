b930da6a52b20ba25a088e4c250e3ddc2fbf6fa3
Alex Eden <alexandereden91@gmail.com>
2019-11-18 20:36:53 -0600

    Cleanup dotstar main

--- example/dotstar-app/src/app/dotstar/dotstar-main.component.ts
+++ example/dotstar-app/src/app/dotstar/dotstar-main.component.ts
@@ -1,41 +1,13 @@
-import { Component, OnInit, OnDestroy, Inject } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Component, Inject } from '@angular/core';
 import { ROUTES, Route } from '@angular/router';
-import { LocalStorage } from '@app/shared';
-
-enum ConfigMenuType {
-  Physics = 'physics',
-  Lights = 'lights',
-  Connection = 'connection',
-}
-
-(window as any).configmenus = ConfigMenuType;
 
 @Component({
   selector: 'dotstar-main',
   templateUrl: './dotstar-main.component.html',
   styleUrls: ['./dotstar-main.component.scss'],
 })
-export class DotstarMainComponent implements OnInit, OnDestroy {
-  private readonly unsubscribe$ = new Subject<any>();
+export class DotstarMainComponent {
   readonly dotstarRoutes: Route[];
-  readonly MenuTypes = ConfigMenuType;
-
-  readonly configMenus = [
-    {
-      type: ConfigMenuType.Lights,
-      icon: 'wb_sunny',
-      label: 'Lights Config',
-    },
-    {
-      type: ConfigMenuType.Connection,
-      icon: 'device_hub',
-      label: 'Connection',
-    },
-  ];
-
-  @LocalStorage()
-  selectedConfigMenuType: ConfigMenuType;
 
   constructor(
     @Inject(ROUTES) public injectedRoutes: Route[][]
@@ -43,24 +15,5 @@ export class DotstarMainComponent implements OnInit, OnDestroy {
     const dotstarParentRoute = this.injectedRoutes[0].find(r => r.path === 'dotstar');
     if (!dotstarParentRoute) throw new Error(`No parent route wth path "/dotstar" found!`);
     this.dotstarRoutes = (dotstarParentRoute.children || []).filter(r => r.path!.length > 0);
-
-    this.selectedConfigMenuType = this.selectedConfigMenuType || ConfigMenuType.Physics;
-  }
-
-  get selectedConfigMenu() {
-    return this.configMenus.find(m => m.type === this.selectedConfigMenuType);
-  }
-
-  selectMenuType(type: ConfigMenuType) {
-    this.selectedConfigMenuType = type;
-  }
-
-  ngOnInit() {
-    //
-  }
-
-  ngOnDestroy() {
-    this.unsubscribe$.next('unsubscribe!');
-    this.unsubscribe$.unsubscribe();
   }
 }
