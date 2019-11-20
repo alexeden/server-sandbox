1f996adb9607fe5637f5b25e8e5c4721b9d9aec0
Alex Eden <alexandereden91@gmail.com>
2019-11-19 23:40:19 -0600

    Turn on TS strict

--- example/dotstar-app/src/app/tetrahedron/lib/tetrahedron.ts
+++ example/dotstar-app/src/app/tetrahedron/lib/tetrahedron.ts
@@ -1,11 +1,22 @@
 import { TetrahedronConfigOptions } from '.';
-import { TetrahedronConfig } from './types';
+import { TetrahedronConfig, Vertex, Edge, Pixel } from './types';
 import { TetrahedronUtils } from './utils';
 
 export class Tetrahedron {
   static fromConfigOptions(configOptions: TetrahedronConfigOptions): Tetrahedron {
     return new Tetrahedron(TetrahedronUtils.configFromOptions(configOptions));
   }
+
+  readonly vertices: Vertex[];
+  /**
+   *
+   */
+  readonly edges: Edge[];
+  /**
+   *
+   */
+  readonly pixels: Pixel[];
+
 
   private constructor(
     readonly config: TetrahedronConfig
