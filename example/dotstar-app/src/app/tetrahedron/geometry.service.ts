1b4efc35389ec90bdfa2e628b9f822eaac608e14
Alex Eden <alexandereden91@gmail.com>
2019-11-19 19:29:33 -0600

    Implement a tet computation stream

--- example/dotstar-app/src/app/tetrahedron/geometry.service.ts
+++ example/dotstar-app/src/app/tetrahedron/geometry.service.ts
@@ -1,6 +1,23 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { TetrahedronConfig, Tetrahedron } from './lib';
+import { shareReplay, map } from 'rxjs/operators';
+import { TetrahedronUtils } from './lib/utils';
 
 @Injectable()
 export class GeometryService {
+  private readonly tetraConfig$ = new BehaviorSubject<TetrahedronConfig>({
+    density: 96 / 1000,
+    edgePadding: 5,
+    paddedEdgeLength: 1010,
+  });
 
+  readonly tetra: Observable<Tetrahedron>;
+
+  constructor() {
+    this.tetra = this.tetraConfig$.pipe(
+      map(config => TetrahedronUtils.computeFromConfig(config)),
+      shareReplay(1)
+    );
+  }
 }
