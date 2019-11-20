f638ef6cbfe8c7a48f8d64c231a8bbcd0863266b
Alex Eden <alexandereden91@gmail.com>
2019-11-19 23:45:10 -0600

    Finish the tet model constructor

--- example/dotstar-app/src/app/tetrahedron/lib/tetrahedron.ts
+++ example/dotstar-app/src/app/tetrahedron/lib/tetrahedron.ts
@@ -8,19 +8,25 @@ export class Tetrahedron {
   }
 
   readonly vertices: Vertex[];
-  /**
-   *
-   */
   readonly edges: Edge[];
-  /**
-   *
-   */
   readonly pixels: Pixel[];
 
 
   private constructor(
     readonly config: TetrahedronConfig
   ) {
+    this.vertices = TetrahedronUtils.verticesFromConfig(this.config);
+    const [ A, B, C, D ] = this.vertices;
 
+    this.edges = [...Array(6).keys()].flatMap(i => [
+      TetrahedronUtils.edgeFromVertices(A, B, i),
+      TetrahedronUtils.edgeFromVertices(A, C, i),
+      TetrahedronUtils.edgeFromVertices(A, D, i),
+      TetrahedronUtils.edgeFromVertices(B, C, i),
+      TetrahedronUtils.edgeFromVertices(B, D, i),
+      TetrahedronUtils.edgeFromVertices(C, D, i),
+    ]);
+
+    this.pixels = this.edges.flatMap(edge => TetrahedronUtils.pixelsFromEdge(edge, this.config));
   }
 }
