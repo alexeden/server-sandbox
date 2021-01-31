dc6c79fe3aa002ae696e7b59450f92ee5d70b4e6
Alex Eden <alexandereden91@gmail.com>
2021-01-30 23:49:39 -0600

    Rename APA102 --> Protocol

--- src/protocol.ts
+++ src/protocol.ts
@@ -1,4 +1,4 @@
-export class APA102C {
+export class Protocol {
   static readonly CLK_MIN = 800_000; // Hz
   static readonly CLK_MAX = 1_200_000; // Hz
   static readonly START_BYTE = 0x00;
@@ -6,9 +6,9 @@ export class APA102C {
   static readonly GLOBAL_BYTE = 0xff;
   static readonly FRAME_SIZE = 4; // bytes
   static readonly startFrame = () =>
-    Buffer.allocUnsafe(APA102C.FRAME_SIZE).fill(APA102C.START_BYTE);
+    Buffer.allocUnsafe(Protocol.FRAME_SIZE).fill(Protocol.START_BYTE);
   static readonly endFrame = () =>
-    Buffer.allocUnsafe(APA102C.FRAME_SIZE).fill(APA102C.END_BYTE);
+    Buffer.allocUnsafe(Protocol.FRAME_SIZE).fill(Protocol.END_BYTE);
   static readonly ledFrame = () =>
-    Buffer.allocUnsafe(APA102C.FRAME_SIZE).fill(APA102C.GLOBAL_BYTE, 0, 1);
+    Buffer.allocUnsafe(Protocol.FRAME_SIZE).fill(Protocol.GLOBAL_BYTE, 0, 1);
 }
