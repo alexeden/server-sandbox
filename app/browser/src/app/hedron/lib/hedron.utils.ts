d5d7cbafc1cde98d92bdcd1b1635eaab3f3983eb
Alex Eden <alexandereden91@gmail.com>
2021-02-01 02:17:23 -0600

    Add calculation for a hedron's N

--- app/browser/src/app/hedron/lib/hedron.utils.ts
+++ app/browser/src/app/hedron/lib/hedron.utils.ts
@@ -3,30 +3,35 @@ import { GeometryData } from './geometry.types';
 import { Hedron } from './hedron.types';
 
 export class HedronUtils {
-  static hedronFromGeometryData({ name, edges }: GeometryData): Hedron {
+  static hedronFromGeometryData({
+    name,
+    edges: edgeData,
+  }: GeometryData): Hedron {
+    const edges = edgeData.map(([vertex0, vertex1, n], index) => {
+      const v0 = new Vector3().fromArray(vertex0);
+      const v1 = new Vector3().fromArray(vertex1);
+      const midpoint = new Line3(v0, v1).getCenter(new Vector3(0, 0, 0));
+
+      return {
+        index,
+        midpoint,
+        leds: HedronUtils.ticks(v0, v1, n).map((p, edgeIndex) => ({
+          p,
+          edgeIndex,
+          hedronIndex: 0,
+          dOrigin: p.length(),
+          dMidpoint: Math.abs(p.distanceTo(midpoint)),
+        })),
+        n,
+        v0,
+        v1,
+      };
+    });
+
     return {
       name,
-      edges: edges.map(([vertex0, vertex1, n], index) => {
-        const v0 = new Vector3().fromArray(vertex0);
-        const v1 = new Vector3().fromArray(vertex1);
-        const midpoint = new Line3(v0, v1).getCenter(new Vector3(0, 0, 0));
-
-        return {
-          index,
-          midpoint,
-          leds: HedronUtils.ticks(v0, v1, n).map((p, edgeIndex) => ({
-            p,
-            edgeIndex,
-            hedronIndex: 0,
-            dOrigin: p.length(),
-            dMidpoint: Math.abs(p.distanceTo(midpoint)),
-          })),
-          n,
-          v0,
-          v1,
-        };
-      }),
-      n: 0,
+      edges,
+      n: edges.reduce((sum, { n }) => sum + n, 0),
     };
   }
 
